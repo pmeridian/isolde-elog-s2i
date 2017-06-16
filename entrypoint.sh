8c0b3250a76361c9ccda6d68b8c4303d275a1c28
Liam Gaffney <lpgaff@gmail.com>
2017-06-16 21:50:34 +0200

    Fix problem with elogd daemon

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Start postfix server
-#systemctl start postfix
-
 # Run elog, for testing
-elogd -p 9090 -c /etc/elogd.cfg -D
+#elogd -p 9090 -c /etc/elogd.cfg -D
 
 # Run elog, for real
-#elogd -p 9090 -c /elog-nfs/elogd.cfg
+elogd -p 9090 -c /elog-nfs/elogd.cfg
